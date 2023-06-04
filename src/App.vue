a6c1c5dd670e32988f4c8d9fbd19421c39532232
adrian <karatsumo@gmail.com>
2023-06-04 14:47:25 +0200

    ajustes

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,37 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { getTradeSeachQuery } from '@/plugins/trade'
+import { getTradeSeachQuery } from '@/utils/trade'
 
-const query = {
-  query: {
-    filters: {
-      trade_filters: {
-        disabled: false,
-        filters: {
-          price: {
-            min: 1,
-            max: 300
-          }
-        }
-      }
-    },
-    status: {
-      option: 'online'
-    },
-    stats: [
-      {
-        type: 'and',
-        filters: []
-      }
-    ],
-    name: "Shavronne's Wrappings",
-    type: "Occultist's Vestment"
-  },
-  sort: {
-    price: 'asc'
-  }
-}
-const response = getTradeSeachQuery(query)
-
-console.log(response)
+// const query = {
+//   query: {
+//     filters: {
+//       trade_filters: {
+//         disabled: false,
+//         filters: {
+//           price: {
+//             min: 1,
+//             max: 300
+//           }
+//         }
+//       }
+//     },
+//     status: {
+//       option: 'online'
+//     },
+//     stats: [
+//       {
+//         type: 'and',
+//         filters: []
+//       }
+//     ],
+//     name: "Shavronne's Wrappings",
+//     type: "Occultist's Vestment"
+//   },
+//   sort: {
+//     price: 'asc'
+//   }
+// }
+// const response = getTradeSeachQuery(query)
 </script>
 
 <template>
